412ad1300c1cb92bdefb28770d0d19effc878826
V <inv1221@yahoo.com>
2012-02-17 18:44:48 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salon3::Application.config.secret_token = 'f23f860acf1e302bade24dd7da08d194abcc2684dba5a94380bcf937e68d9652683277dd677dd989a63533cb7408e69f58c1987d6d210fa62996484240061293'
